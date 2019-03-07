f816e82147f6e6dd6cd438e0adc3e69011c7c3d3
Ingo <roessner@infai.org>
2019-03-07 10:07:33 +0100

    hub api uses jwtToken as value

--- iot/hub.go
+++ iot/hub.go
@@ -24,7 +24,7 @@ import (
 	"net/url"
 )
 
-func (this *Iot) GetHub(id string, cred *security.JwtToken) (hub model.Hub, err error) {
+func (this *Iot) GetHub(id string, cred security.JwtToken) (hub model.Hub, err error) {
 	resp, err := cred.Get(this.url + "/hub/" + url.QueryEscape(id))
 	if err != nil {
 		log.Println("ERROR on GetGateway()", err)
@@ -39,53 +39,53 @@ func (this *Iot) GetHub(id string, cred *security.JwtToken) (hub model.Hub, err 
 	return hub, err
 }
 
-func (this *Iot) GetHubHash(id string, cred *security.JwtToken) (hash string, err error) {
+func (this *Iot) GetHubHash(id string, cred security.JwtToken) (hash string, err error) {
 	err = cred.GetJSON(this.url+"/hub/"+url.QueryEscape(id)+"/hash", &hash)
 	return
 }
 
-func (this *Iot) GetHubName(id string, cred *security.JwtToken) (name string, err error) {
+func (this *Iot) GetHubName(id string, cred security.JwtToken) (name string, err error) {
 	err = cred.GetJSON(this.url+"/hub/"+url.QueryEscape(id)+"/name", &name)
 	return
 }
 
-func (this *Iot) GetHubDevices(id string, cred *security.JwtToken) (devices []string, err error) {
+func (this *Iot) GetHubDevices(id string, cred security.JwtToken) (devices []string, err error) {
 	err = cred.GetJSON(this.url+"/hub/"+url.QueryEscape(id)+"/devices", &devices)
 	return
 }
 
-func (this *Iot) CreateHub(id string, hub model.Hub, cred *security.JwtToken) (result model.Hub, err error) {
+func (this *Iot) CreateHub(id string, hub model.Hub, cred security.JwtToken) (result model.Hub, err error) {
 	err = cred.PostJSON(this.url+"/hub", hub, &result)
 	return
 }
 
-func (this *Iot) ExistsHub(id string, cred *security.JwtToken) (exists bool, err error) {
+func (this *Iot) ExistsHub(id string, cred security.JwtToken) (exists bool, err error) {
 	exists, err = cred.Head(this.url + "/hub/" + url.QueryEscape(id))
 	return
 }
 
-func (this *Iot) UpdateHub(id string, hub model.Hub, cred *security.JwtToken) (result model.Hub, err error) {
+func (this *Iot) UpdateHub(id string, hub model.Hub, cred security.JwtToken) (result model.Hub, err error) {
 	hub.Id = id
 	err = cred.PutJSON(this.url+"/hub/"+url.QueryEscape(id), hub, &result)
 	return
 }
 
-func (this *Iot) UpdateHubHash(id string, hash string, cred *security.JwtToken) (err error) {
+func (this *Iot) UpdateHubHash(id string, hash string, cred security.JwtToken) (err error) {
 	err = cred.PutJSON(this.url+"/hub/"+url.QueryEscape(id)+"/hash", hash, nil)
 	return
 }
 
-func (this *Iot) UpdateHubName(id string, name string, cred *security.JwtToken) (err error) {
+func (this *Iot) UpdateHubName(id string, name string, cred security.JwtToken) (err error) {
 	err = cred.PutJSON(this.url+"/hub/"+url.QueryEscape(id)+"/name", name, nil)
 	return
 }
 
-func (this *Iot) UpdateHubDevices(id string, devices []string, cred *security.JwtToken) (err error) {
+func (this *Iot) UpdateHubDevices(id string, devices []string, cred security.JwtToken) (err error) {
 	err = cred.PutJSON(this.url+"/hub/"+url.QueryEscape(id)+"/devices", devices, nil)
 	return
 }
 
-func (this *Iot) DeleteHub(id string, cred *security.JwtToken) (err error) {
+func (this *Iot) DeleteHub(id string, cred security.JwtToken) (err error) {
 	_, err = cred.Delete(this.url + "/hub/" + url.QueryEscape(id))
 	return
 }
